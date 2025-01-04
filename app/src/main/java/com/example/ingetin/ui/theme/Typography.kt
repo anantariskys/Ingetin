86e57d18c7992ea2750105a74f631d4474f8d705
anantariskys <gamersindo555@gmail.com>
2025-01-04 18:21:54 +0700

    feat : add typography configuration

---
+++ app/src/main/java/com/example/ingetin/ui/theme/Typography.kt
@@ -0,0 +1,7 @@
+package com.example.ingetin.ui.theme
+
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.googlefonts.GoogleFont
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.font.FontWeight
+

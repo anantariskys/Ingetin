9752c36a747cd1e8ffd522eab3eefc2b7656bc83
anantariskys <gamersindo555@gmail.com>
2025-01-04 18:21:31 +0700

    feat(screen) : add login

---
+++ app/src/main/java/com/example/ingetin/ui/screens/LoginScreen.kt
@@ -0,0 +1,34 @@
+package com.example.ingetin.ui.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.material3.Button
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun LoginScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Login to Inget.in",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Button(onClick = { /* Implement your login action */ }) {
+            Text(text = "Login")
+        }
+    }
+}

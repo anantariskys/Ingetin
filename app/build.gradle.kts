e482d220819cbdc5fa994ab922bb6d6c2b528215
anantariskys <gamersindo555@gmail.com>
2025-01-06 09:35:34 +0700

    feat : add extended icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ android {
 }
 
 dependencies {
+    implementation ("androidx.compose.material:material-icons-core:1.5.0")
+    implementation ("androidx.compose.material:material-icons-extended:1.5.0")
     implementation ("androidx.navigation:navigation-compose:2.6.0")
     implementation ("androidx.compose.ui:ui-text-google-fonts")
     implementation(libs.androidx.core.ktx)
